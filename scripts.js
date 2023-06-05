cad08b130667ff3bc36ed1d561a10839fadbc927
laumazzotta <lau.mazzotta@gmail.com>
2023-06-05 10:21:45 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+jQuery(function($) {
+
+    $('.hero-title > h1 > span').addClass('animate__fadeInLeft');
+
+    $(window).scroll(function(a, b, c) {
+        const offset = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
+        $('body').css('--scroll', offset);
+
+        
+    });
+
+});
+
+let navButton = document.getElementById('nav-icon3');
+let navMobile = document.getElementById('myNav');
+let brand = document.getElementById('brand');
+
+navButton.onclick = function() {
+    this.classList.toggle('open');
+    navMobile.classList.toggle('h-100');
+    brand.classList.toggle('width-100px');
+
+}
