c4e0999711a8e49c4279413659f0cf0631a20696
laumazzotta <lau.mazzotta@gmail.com>
2023-06-09 11:07:56 -0300

    navbar

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
 jQuery(function($) {
 
-    $('.hero-title > h1 > span').addClass('animate__fadeInLeft');
-
     $(window).scroll(function(a, b, c) {
         const offset = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
         $('body').css('--scroll', offset);
@@ -11,14 +9,26 @@ jQuery(function($) {
 
 });
 
-let navButton = document.getElementById('nav-icon3');
-let navMobile = document.getElementById('myNav');
-let brand = document.getElementById('brand');
+// let navButton = document.getElementById('nav-icon3');
+// let navMobile = document.getElementById('myNav');
+// let brand = document.getElementById('brand');
+// let navLinks = document.getElementById('navLinks');
 
-navButton.onclick = function() {
-    this.classList.toggle('open');
-    navMobile.classList.toggle('h-100');
-    brand.classList.toggle('width-100px');
+// navButton.onclick = function() {
+//     this.classList.toggle('open');
+//     navMobile.classList.toggle('h-100');
+//     brand.classList.toggle('width-100px');
 
-}
+// }
 
+$('#openNav').on('click', function() {
+    $('#myNav').css("height", "100%");
+});
+
+$('#closeNav').on('click', function() {
+    $('#myNav').css("height", "0%");
+});
+
+$('.nav-link').on('click', function() {
+    $('#myNav').css("height", "0%");
+});
